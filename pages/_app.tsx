9c807fb175e448e34161e50f8e5041aa8459c259
Umaha Tokula <umahatokula@gmail.com>
2023-01-25 08:11:13 +0100

    Migrate Nav and Footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,6 +6,8 @@ import { configureChains, createClient, WagmiConfig } from 'wagmi';
 import { polygonMumbai } from 'wagmi/chains';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
+import Nav from '../components/Nav'
+import Footer from '../components/Footer'
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
@@ -37,7 +39,13 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
-        <Component {...pageProps} />
+        <div  className='h-screen bg-vote-50 pb-12'>
+          <Nav />
+          <div className='w-11/12 md:w-6/12 h-full mx-auto pt-12'>
+            <Component {...pageProps} />
+          </div>
+          <Footer />
+        </div>
       </RainbowKitProvider>
     </WagmiConfig>
   );
